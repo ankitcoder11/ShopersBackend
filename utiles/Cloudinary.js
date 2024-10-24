413530e76cf4392f0c868296fc65a9a690b19a88
ankitcoder11 <ankitakcp@gmail.com>
2024-10-24 11:21:06 +0530

    many things has been completed

--- utiles/Cloudinary.js
+++ utiles/Cloudinary.js
@@ -9,7 +9,6 @@ const uploadOnCloudinary = async (localFilePath) => {
     try {
 
         if (!localFilePath) return null
-
         const response = await cloudinary.uploader.upload(
             localFilePath, {
             resource_type: 'auto'
